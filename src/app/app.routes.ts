590840ef13ab1c53a4c54aef1bab0028169309d6
codekeshri <arvidce@gmail.com>
2024-10-27 11:03:29 +0530

    ems

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'employee',
+                component: EmployeeComponent
+            },
+            {
+                path: 'projects',
+                component: ProjectsComponent
+            },
+        ]
+    },
+
+
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginObj: any = {
+    username: '',
+    password: '',
+
+  }
+
+  router = inject(Router)
+
+
+  loginHandler(){
+    console.log('login')
+    if(this.loginObj.username === 'admin' && this.loginObj.password === '112233'){
+      this.router.navigateByUrl('/dashboard')
+    }else{
+      alert("Username and Password Incorrect!")
+    }
+  }
+
+}
